150a219c56112633ee1def091037aab8a5bc5ab6
Shell Chen <me@sorz.org>
2021-09-30 14:47:30 +0800

    fix non-canonical url matching

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
                     <Col xs={12} sm={11} md={9} lg={8}>
                         <Switch>
                             <Route exact path="/"><Link to="/askee/1/ask">提问</Link></Route>
-                            <Route exact path="/askee/:askeeId(\d+)/ask" component={SubmitForm} />
+                            <Route exact path="/askee/:askeeId([1-9]\d*)/ask" component={SubmitForm} />
                             <Route exact path="*">404 - 页面未找到</Route>
                         </Switch>
                     </Col>
